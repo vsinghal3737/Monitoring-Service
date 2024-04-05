6fc3cf6530348ca8adde6cb95ca4eff8f97cdd1f
Vaibhav Singhal <vsinghal3737@gmail.com>
2024-04-05 02:22:44 -0400

    basic ut caller

---
+++ test/models/TestCaller.py
@@ -0,0 +1,18 @@
+import unittest
+from datetime import timedelta
+
+from src.models.Caller import Caller
+
+
+class TestCaller(unittest.TestCase):
+
+    def test_subscription(self):
+        caller = Caller('John Doe', 'caller1')
+        caller.add_subscription('127.0.0.1', 8080, timedelta(seconds=30))
+        self.assertIn(('127.0.0.1', 8080), caller.subscribed)
+        caller.remove_subscription('127.0.0.1', 8080)
+        self.assertNotIn(('127.0.0.1', 8080), caller.subscribed)
+
+
+if __name__ == '__main__':
+    unittest.main()
