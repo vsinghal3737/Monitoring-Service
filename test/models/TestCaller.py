00e6642a09927a13f9fe520cdced14e1624508f4
Vaibhav Singhal <vsinghal3737@gmail.com>
2024-04-06 00:35:15 -0400

    Update TestCaller.py

--- test/models/TestCaller.py
+++ test/models/TestCaller.py
@@ -9,6 +9,8 @@ class TestCaller(unittest.TestCase):
     def test_subscription(self):
         caller = Caller('John Doe', 'caller1')
         caller.add_subscription('127.0.0.1', 8080, timedelta(seconds=30))
+        caller.add_subscription('127.0.0.2', 8082, timedelta(seconds=30))
+        self.assertTrue(caller.subscribed[('127.0.0.2', 8082)])
         self.assertIn(('127.0.0.1', 8080), caller.subscribed)
         caller.remove_subscription('127.0.0.1', 8080)
         self.assertNotIn(('127.0.0.1', 8080), caller.subscribed)
