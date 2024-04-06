0e724120ec9743b17aeb702ba97e8056b2ef8352
Vaibhav Singhal <vsinghal3737@gmail.com>
2024-04-05 23:47:20 -0400

    main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,56 @@
-# This is a sample Python script.
+import threading
+import signal
+from MonitorService import MonitorService
+from E2E.DummyServices import PORTS, DummyServiceMain
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+shutdown_event = threading.Event()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def start_dummy_services():
+    # Start DummyServices and pass the shutdown event
+    services = DummyServiceMain(shutdown_event)
+    return services
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def setup_monitor_service():
+    # Initialize MonitorService
+    return MonitorService(5, shutdown_event)
+
+
+def setup_config_service(monitor_service):
+    # Register services
+    for port in PORTS:
+        monitor_service.register_service("localhost", port)
+
+    # Register callers
+    monitor_service.register_caller("Caller 1", "caller1")
+    monitor_service.register_caller("Caller 2", "caller2")
+
+    # Subscribe callers to services with specified polling frequencies
+    monitor_service.subscribe_service("localhost", PORTS[0], "caller1", polling_frequency=3)
+    monitor_service.subscribe_service("localhost", PORTS[1], "caller1", polling_frequency=10)
+    monitor_service.subscribe_service("localhost", PORTS[1], "caller2", polling_frequency=10)
+    monitor_service.subscribe_service("localhost", PORTS[2], "caller2", polling_frequency=10)
+
+
+def shutdown_handler(signum, frame):
+    # Set the shutdown event when a signal is received (e.g., SIGINT for KeyboardInterrupt)
+    shutdown_event.set()
+
+
+def main():
+    signal.signal(signal.SIGINT, shutdown_handler)
+    dummy_service_thread = threading.Thread(target=start_dummy_services)
+    dummy_service_thread.start()
+
+    monitor_service = setup_monitor_service()
+
+    setup_config_service(monitor_service)
+
+    # Wait for the threads to complete upon shutdown signal
+    dummy_service_thread.join()
+    monitor_service.join()  # Make sure to have a join method in MonitorService
+
+
+if __name__ == "__main__":
+    main()
